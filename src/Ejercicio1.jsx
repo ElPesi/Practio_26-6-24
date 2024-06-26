e417ef577faace1f7149480319124c9b076a79c4
Tito Calderon <162237728+ElPesi@users.noreply.github.com>
2024-06-26 11:24:52 -0300

    Update Ejercicio1.jsx

--- src/Ejercicio1.jsx
+++ src/Ejercicio1.jsx
@@ -1,12 +1,23 @@
-export default function Ejercicio1() {
+import { useState } from "react";
+const Ejercicio1 = () => {
+  const [selectData, setSelectData] = useState('');
+
+  const dataChange = (event) => {
+    setSelectData(event.target.value);
+  };
 
   return (
     <div>
       <h2>Ejercicio 1: Lista desplegable</h2>
 
       <label>Seleccione un producto: </label>
-      {/* Crear lista de selección */}
-
+      <select onChange={dataChange}>
+        <option className="Set de Cables" value="$10"> Set de cables</option>
+        <option className="Bateria" value="$50"> Bateria</option>
+        <option className="Optica" value="$100"> Optica</option>
+      </select>
+      <label>Producto:{selectData}</label>
     </div>
   )
 }
+export default Ejercicio1
